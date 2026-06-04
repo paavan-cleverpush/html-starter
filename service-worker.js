a42d2300fb3891127417cd34821de22864beeea7
paavan-cleverpush <p.khunt.ext@cleverpush.com>
2026-06-04 12:05:39 +0530

    fix

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ self.addEventListener('install', (event) => {
         './',
         './index.html',
         './styles.css',
-        './icon-192x192.png',
-        './icon-512x512.png',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
       ]);
     })
   );
